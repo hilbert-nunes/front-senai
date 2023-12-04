ce89cebb69fb1e2a19c044cd7fc8b6e416ec015a
hilbert-nunes <hilbert.nunes@gmail.com>
2023-12-04 19:43:48 -0300

    add login

--- js/jquery-script.js
+++ js/jquery-script.js
@@ -1,1 +1,17 @@
 //efeito de esconder formulário de cadastro
+$(document).ready(function(){
+    $("#botao-cadastrar").click(function(){
+        $("#form-cadastrar").slideToggle("slow");
+        $("#section-login").slideToggle("slow");
+        $("#botao-cadastrar").hide();
+    });
+});
+
+//efeito de esconder formulário de cadastro
+$(document).ready(function(){
+    $("#bt-voltar").click(function(){
+        $("#section-login").slideToggle("slow");
+        $("#form-cadastrar").slideToggle("slow");
+        $("#botao-cadastrar").show();
+    });
+});
